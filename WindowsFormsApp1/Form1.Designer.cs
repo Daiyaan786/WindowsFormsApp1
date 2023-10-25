c16095a5a5883c3ab6822aa67803d34648b0122f
Daiyaan786 <st10082008@vcconnect.edu.za>
2023-10-25 17:14:23 +0200

    update

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -95,12 +95,15 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.facebook;
+            this.button1.Image = global::WindowsFormsApp1.Properties.Resources.facebook;
             this.button1.Location = new System.Drawing.Point(592, 364);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(58, 46);
             this.button1.TabIndex = 11;
             this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
--- WindowsFormsApp1/Properties/Resources.Designer.cs
+++ WindowsFormsApp1/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace WindowsFormsApp1.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap facebook {
+            get {
+                object obj = ResourceManager.GetObject("facebook", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap OIP__2_ {
             get {
                 object obj = ResourceManager.GetObject("OIP (2)", resourceCulture);
